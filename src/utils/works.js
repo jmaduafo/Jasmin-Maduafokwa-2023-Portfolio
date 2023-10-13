f5f7642f2d59897c044a5f94f840131a9a39e4fd
Jay Madu <jmaduafokwa@hotmail.com>
2023-10-13 15:46:38 +0100

    attempting to fix bug in heroku

--- src/utils/works.js
+++ src/utils/works.js
@@ -9,7 +9,7 @@ const selectedWorks = [
       "github": 'jmaduafo/Zaza-Ecommerce-Website',
       "link": 'https://quiet-caverns-99335-f8f9f8c7d100.herokuapp.com',
       "images": ['https://res.cloudinary.com/dyxxn831a/image/upload/v1691871199/zaza-portfolio_kblowd.png', 'https://res.cloudinary.com/dyxxn831a/image/upload/v1691764188/portfolio%20images/pixie18_11_w6h2vw.jpg'],
-      "landscapeImage": "https://res.cloudinary.com/dyxxn831a/image/upload/v1691764224/portfolio%20images/fashion-photo-seductive-model-with-perfect-body-posing-tropical-beach-sunset-colots_ol2r9j.jpg",
+      "landscapeImage": "https://res.cloudinary.com/dyxxn831a/image/upload/v1697207343/portfolio%20images/fashion-photo-seductive-model-with-perfect-body-posing-tropical-beach-sunset-colots_1_anjx7o.jpg",
       "opening": 'The ultimate destination for exquisite lingerie and fragrances that celebrate the essence of femininity.',
       "projectDescription": "Our handpicked collection of lingerie, pajamas, bras & panties are thoughtfully designed and selected to complement the customer’s personality and preferences perfectly with a wide array of sizes, styles, and colors.",
       "backgroundDescription": "As a group project, I collaborated with two other women in my bootcamp to make Zaza come to life, a full-stack lingerie and fragrance store. Our intention was to create a site geared towards primarily women and female sensuality. The goal was to make the site look real, professional, and authentic and to include stripe for card payments. We each had our own responsibilities in the project, and it was through good communication, frequent updates and meetings, and togetherness that we were able to complete the project by the deadline. In the future, we seek to include men's wear for more inclusivity as well as customer review sections per product displayed."
@@ -24,7 +24,7 @@ const selectedWorks = [
       "github": 'baud-labs/company-website',
       "link": 'https://www.baudlabs.com.ng/',
       "images": ['https://res.cloudinary.com/dyxxn831a/image/upload/v1691882659/7e4b7dd036a_1_qzackk.png'],
-      "landscapeImage": "https://res.cloudinary.com/dyxxn831a/image/upload/v1691764844/portfolio%20images/phone-screen-with-abstract-marble-aesthetic_xomj7a.jpg",
+      "landscapeImage": "https://res.cloudinary.com/dyxxn831a/image/upload/v1697207351/portfolio%20images/phone-screen-with-abstract-marble-aesthetic_1_rmra8z.jpg",
       "opening": 'Organizations trust us and our world-class intelligent software solutions to make proficient enhancements.',
       "projectDescription": "Baud Labs improves the lives of customers by providing simple solutions to complex problems. They infuse IT into complex organizational processes simplifying them for world class customer experience, transparency & higher ROI.",
       "backgroundDescription": "This application was designed and developed by myself as part of my role as a frontend intern at Baud Technologies. Being a bit of a perfectionist, I wanted the website to look good and professional and to match the overall feel of what my employer was going for. His desire was for his company website to be a single-page website with a scientific, AI feel. I incorporated the requirements that he set, but I also wanted to elevate it using GSAP, adding a bit of my own spin visually. I was thinking about Star Trek and UFOs while designing the site, and as a result, my employer was extremely satisfied with the end product."
@@ -39,7 +39,7 @@ const selectedWorks = [
       "github": 'robobrownie13/animal-blog-group-4-p2',
       "link": 'https://salty-badlands-43491.herokuapp.com/',
       "images": ['https://res.cloudinary.com/dyxxn831a/image/upload/v1691883406/oasis-blog_uezxjd.png'],
-      "landscapeImage": "https://res.cloudinary.com/dyxxn831a/image/upload/v1691883563/animal-lizard-nature-multi-colored-close-up-generative-ai_edghie.jpg",
+      "landscapeImage": "https://res.cloudinary.com/dyxxn831a/image/upload/v1697207909/portfolio%20images/animal-lizard-nature-multi-colored-close-up-generative-ai_1_rcfsda.jpg",
       "opening": 'Immerse yourself in a vibrant community dedicated to protecting endangered animals. ',
       "projectDescription": "This website is more than just a platform-it’s a hub where passionate individuals unite to discuss the state of vulnerable, endangered creatures.",
       "backgroundDescription": "Oasis is an interactive, sociable, and informative blog site regarding endangered animals where users alike can discuss many different topics by creating posts, share information from external sites through comments, and create accounts of their own in order to gain extended knowledge, current data, and support from many users all across the globe. My team didn't just want to make things functional and make sure that things were working well in the frontend and the backend, but we also sought to solve a problem with our project, which is where the power of coding comes in. In the future, our objective is to allow users the opportunity to follow other users and favorite posts to keep a steady record of posts and users that inspire."
